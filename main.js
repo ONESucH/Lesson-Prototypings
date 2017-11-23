c98e127f049638fb709588e07647b2454916041f
Bogdanov Ilya <onesuch07@gmail.com>
2017-11-23 15:13:45 +0300

    the lesson is fixed

--- main.js
+++ main.js
@@ -22,30 +22,105 @@
 //};
 
 // Прототипирование
-var UserData = {
-    constructor: function (name, age, gender) { // Создаём объект конструктор
+// var UserData = {
+//     constructor: function (name, age, gender) { // Создаём объект конструктор
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender;
+//         return this;
+//     },
+//     greet: function () {
+//         console.log('Сообщение для ', this.name);
+//     }
+// };
+//
+// var userOne, userTwo, userThree;
+//
+// userOne = Object.create(UserData).constructor('Вова', 30, 'male');
+// userTwo = Object.create(UserData).constructor('Ваня', 45, 'male');
+// userThree = Object.create(UserData).constructor('Вера', 25, 'female');
+//
+// console.log(userOne.name);
+// console.log(userTwo.age);
+// console.log(userThree.gender);
+//
+// userOne.greet();
+// userTwo.greet();
+// userThree.greet();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var dataUser = {
+    constructor: function (name, age, description) {
         this.name = name;
         this.age = age;
-        this.gender = gender;
+        this.description = description;
         return this;
     },
-    greet: function () {
-        console.log('Сообщение для ', this.name);
+    render: function () {
+        console.log('Ваше имя ', this.name);
+        console.log('Ваш возрост ', this.age);
+        console.log('Ваше описание ', this.description);
     }
 };
 
-var userOne, userTwo, userThree;
+var vova, valya, dima;
 
-userOne = Object.create(UserData).constructor('Вова', 30, 'male');
-userTwo = Object.create(UserData).constructor('Ваня', 45, 'male');
-userThree = Object.create(UserData).constructor('Вера', 25, 'female');
+vova = Object.create(dataUser).constructor('vova', 50, 'Описание vova');
+valya = Object.create(dataUser).constructor('valya', 20, 'Описание valya');
+dima = Object.create(dataUser).constructor('dima', 27, 'Описание dima');
 
-console.log(userOne.name);
-console.log(userTwo.age);
-console.log(userThree.gender);
 
-userOne.greet();
-userTwo.greet();
-userThree.greet();
+console.log(vova);
+console.log(valya);
+console.log(dima);
 
+
+
+vova.render();
+valya.render();
+dima.render();
 
